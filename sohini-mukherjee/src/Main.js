71bf4e3e2bb87e1703117a348011f016e4f0239a
Sohini M <smukherjee@Sohinis-MacBook-Pro.local>
2020-04-05 19:59:34 -0700

    master: Added scss, and bio.

--- sohini-mukherjee/src/Main.js
+++ sohini-mukherjee/src/Main.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import './Main.css';
+import './Main.scss';
 
 const Main = () => {
   return (
     <main id="main" className="main" role="main">
       <section className="bio">
+        <div className="bio--textbox">
+          <h1 className="heading">Hey, I'm Sohini.</h1>
+          <p>
+            I like Math, Physics, building new things, and performance poetry.
+          </p>
+          <p>
+            I’m a Computer Engineering student with minors in Cognitive Science
+            at the&nbsp;
+            <a href="https://uwaterloo.ca/" target="_blank">University of
+              Waterloo
+            </a>
+            <p>
+              I have a keen interest in artificial intelligence and software
+              development.
+            </p>
+
+          </p>
+        </div>
       </section>
 
       <section className="experience">
