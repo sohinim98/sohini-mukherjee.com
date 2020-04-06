946d7685941fd194d05c008bd9a803107de6d25e
Sohini M <smukherjee@Sohinis-MacBook-Pro.local>
2020-04-05 17:25:22 -0700

    master: Created own markup.

---
+++ sohini-mukherjee/src/Main.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Main.css';
+
+const Main = () => {
+  return (
+    <main id="main" className="main" role="main">
+      <section className="bio">
+      </section>
+
+      <section className="experience">
+      </section>
+
+      <section className="projects">
+      </section>
+
+      <section className="blogs">
+      </section>
+    </main>
+  )
+}
+
+export default Main
